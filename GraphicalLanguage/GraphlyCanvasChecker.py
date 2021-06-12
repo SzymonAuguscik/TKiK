52817d0828c0b811fc14abafc04e2ac5fbf0a5bf
Stanisław Światłoch <sswiatloch@hotmail.com>
2021-06-12 19:31:50 +0200

    add SaveBeforeCanvasException

--- GraphicalLanguage/GraphlyCanvasChecker.py
+++ GraphicalLanguage/GraphlyCanvasChecker.py
@@ -1,6 +1,7 @@
 from GraphlyListener import GraphlyListener
 from GraphlyParser import GraphlyParser
 from exceptions.DrawBeforeCanvasException import DrawBeforeCanvasException
+from exceptions.SaveBeforeCanvasException import SaveBeforeCanvasException
 
 
 class GraphlyCanvasChecker(GraphlyListener):
@@ -13,3 +14,11 @@ class GraphlyCanvasChecker(GraphlyListener):
     def enterDraw(self, ctx: GraphlyParser.DrawContext):
         if not self.is_canvas_initialized:
             raise DrawBeforeCanvasException(ctx.start.line)
+
+    def enterSimpleSave(self, ctx:GraphlyParser.SimpleSaveContext):
+        if not self.is_canvas_initialized:
+            raise SaveBeforeCanvasException(ctx.start.line)
+
+    def enterNamedSave(self, ctx:GraphlyParser.NamedSaveContext):
+        if not self.is_canvas_initialized:
+            raise SaveBeforeCanvasException(ctx.start.line)
